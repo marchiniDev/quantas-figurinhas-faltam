f33a655f79adb5105f4262f4af8ed03dbac29125
Renan Marchini <marchini.developer@gmail.com>
2021-02-08 08:49:51 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace GFT_Desafio2_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeFigurinhas = Int32.Parse(Console.ReadLine());
+            int numeroDeFigurinhasCompradas = Int32.Parse(Console.ReadLine());
+            int totalDeFigurinhas = 0;
+
+            int[] albumDeFigurinha = new int[numeroDeFigurinhasCompradas];
+
+            // IMPLEMENTE AQUI A SOLUÇÃO
+
+            //Ler do console número das figurinhas compradas.
+            for (int i = 0; i < numeroDeFigurinhasCompradas; i++)
+            {
+                albumDeFigurinha[i] = int.Parse(Console.ReadLine());
+            }
+
+            //Contar número de figurinhas diferentes.
+            int numeroFigurinhasDiferentes = albumDeFigurinha.Distinct().Count();
+
+            //Calcular número de figurinhas restantes.
+            totalDeFigurinhas = numeroDeFigurinhas - numeroFigurinhasDiferentes;
+            
+            //Escrever no console o número de figurinhas faltantes
+            Console.WriteLine(totalDeFigurinhas);
+        }
+    }
+}
